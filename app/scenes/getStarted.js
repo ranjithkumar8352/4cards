dcce97db2dc916f70e2586f7de78abfaf527c4bd
Ranjith <ranjithkumar8352@gmail.com>
2017-08-12 01:17:12 +0530

    final changes

--- app/scenes/getStarted.js
+++ app/scenes/getStarted.js
@@ -7,9 +7,9 @@ export default class GetStarted extends Component{
   render(){
       return (
           <View style={{flex:1,backgroundColor:'#9050FE',justifyContent:'space-around',alignItems:'center',paddingVertical:10}}>
-              <Text style={{paddingHorizontal:20,color:'white',textAlign:'center'}}>Most simple & fun way to improve english vocabulary</Text>
+              <Text style={{paddingHorizontal:20,color:'white',textAlign:'center'}}>Most simple & fun way to learn great words for better spoken english.</Text>
               <Image source={require('../images/intro-minion.png')} style={{width:200,height:200}} />
-              <Text style={{paddingHorizontal:20,color:'white',textAlign:'center'}}>Most simple & fun way to improve english vocabulary</Text>
+              <Text style={{paddingHorizontal:20,color:'white',textAlign:'center'}}>Learn 1 word at a time, 4 words in a day, 3600 words in a year</Text>
               <TouchableOpacity onPress={()=>{store.save('introShown',true).then(()=>Actions.splash({type:'reset'}))}}>
                   <Text style={{
                       fontSize: 14,
